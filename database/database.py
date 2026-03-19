3ae229d8beb7f22d272b2c1cab74d2aab6c1b5d9
Priyanshu Raj <pr590315@gmail.com>
2026-03-19 18:09:50 +0530

    just bring the weather in the database

--- database/database.py
+++ database/database.py
@@ -1,1 +1,30 @@
-print("hello world")
+# importing packages
+import requests
+import json
+import time
+web_wurl = "XXX"
+web_turl = "XXX"
+r = requests.get(web_wurl)
+print(r.json())
+def weather_api(latitude, longitude):
+    wurl = f"https://api.open-meteo.com/v1/forecast?latitude={data['latitude']}&longitude={data['longitude']}&current=temperature_2m,weather_code&forecast_days=1"
+    weather_data = requests.get(wurl).json()
+    print(weather_data)
+    w_data = (weather_data['current'])
+    print(w_data)
+    weather_code = w_data['weather_code']
+    temperature = w_data['temperature_2m']
+    print(f"Temperature: {w_data['temperature_2m']}")
+    print(f"Weather Code: {w_data['weather_code']}")
+    time.sleep(10 * 60)
+    return
+
+for api_id, data in r.json().items():
+
+    while True:
+        try:
+            weather_api(data['latitude'], data['longitude'])
+
+        except:
+            print("API Error")
+            time.sleep(5)
